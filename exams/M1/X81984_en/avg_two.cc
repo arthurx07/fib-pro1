9e9bc6fd68740992fd6a956a46a441a70cf4a2e4
kumao <arturleivarx@gmail.com>
2023-11-01 17:45:36 +0100

    exams/M1/X81984_en remove unnecessary if()

--- exams/M1/X81984_en/avg_two.cc
+++ exams/M1/X81984_en/avg_two.cc
@@ -6,15 +6,10 @@ using namespace std;
 
 int main() {
   int a,b,c; // we work with real numbers (avg divisions); though we could also use the module
-  bool found = false;
   cin >> a >> b >> c;
 
-  if ((a + b)/2.0 == c) found = true;
-  else if ((a + c)/2.0 == b) found = true;
-  else if ((b + c)/2.0 == a) found = true;
-
-  if (found) cout << "YES" << endl;
+  if ((a + b)/2.0 == c or (a + c)/2.0 == b or (b + c)/2.0 == a) cout << "YES" << endl;
   else cout << "NO" << endl;
 }
 
-// if input is integer, can't read them as double
+// if input is integer, shouldn't read it as double
